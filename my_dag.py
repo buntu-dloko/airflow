1f45a3da9011e71b88dd15bb2363a333e31b870f
buntu-dloko <buntu.dloko@absa.africa>
2022-06-06 17:22:10 +0200

    Spark_Submit

--- my_dag.py
+++ my_dag.py
@@ -2,55 +2,50 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 
-from random import randint
 from datetime import datetime
+from random import randint
 
-def _choose_best_model(ti):
+def _choosing_best_model(ti):
     accuracies = ti.xcom_pull(task_ids=[
         'training_model_A',
         'training_model_B',
         'training_model_C'
-    ])
-    best_accuracy = max(accuracies)
-    if (best_accuracy > 8):
+        ])
+    if max(accuracies) > 8:
         return 'accurate'
     return 'inaccurate'
 
-
-def _training_model():
+def _training_model(model):
     return randint(1, 10)
 
-with DAG("my_dag", start_date=datetime(2021, 1, 1),
-    schedule_interval="@daily", catchup=False) as dag:
+with DAG("test",
+    start_date=datetime(2021, 1 ,1), 
+    schedule_interval='@daily', 
+    catchup=False) as dag:
 
-        training_model_A = PythonOperator(
-            task_id="training_model_A",
-            python_callable=_training_model
-        )
+    training_model_tasks = [
+        PythonOperator(
+            task_id=f"training_model_{model_id}",
+            python_callable=_training_model,
+            op_kwargs={
+                "model": model_id
+            }
+        ) for model_id in ['A', 'B', 'C']
+    ]
 
-        training_model_B = PythonOperator(
-            task_id="training_model_B",
-            python_callable=_training_model
-        )
+    choosing_best_model = BranchPythonOperator(
+        task_id="choosing_best_model",
+        python_callable=_choosing_best_model
+    )
+    
+    accurate = BashOperator(
+        task_id="accurate",
+        bash_command="echo 'accurate'"
+    )
 
-        training_model_C = PythonOperator(
-            task_id="training_model_C",
-            python_callable=_training_model
-        )
+    inaccurate = BashOperator(
+        task_id="inaccurate",
+        bash_command=" echo 'inaccurate'"
+    )
 
-        choose_best_model = BranchPythonOperator(
-            task_id="choose_best_model",
-            python_callable=_choose_best_model
-        )
-
-        accurate = BashOperator(
-            task_id="accurate",
-            bash_command="echo 'accurate'"
-        )
-
-        inaccurate = BashOperator(
-            task_id="inaccurate",
-            bash_command="echo 'inaccurate'"
-        )
-
-        [training_model_A, training_model_B, training_model_C] >> choose_best_model >> [accurate, inaccurate]
+    training_model_tasks >> choosing_best_model >> [accurate, inaccurate]
